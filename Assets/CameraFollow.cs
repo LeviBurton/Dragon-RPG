5b3ed3285fcc76b8995b3c6fefc6fc80ddc91167
Levi Burton <levi.burton@outlook.com>
2017-12-28 22:51:19 -0600

    initial import

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject Player = null;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        if (Player != null)
+        {
+            transform.position = Player.transform.position;
+        }
+    }
+}
