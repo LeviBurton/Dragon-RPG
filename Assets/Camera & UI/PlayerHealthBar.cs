5126363fda1263a5e547f302e07f56f0be5dd527
Levi Burton <levi.burton@outlook.com>
2017-12-31 14:43:21 -0600

    S2 L29 Player Health Bar UI

---
+++ Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PlayerHealthBar : MonoBehaviour
+{
+    // Image specs
+    // health bar: 2048x32
+    // health bar mask: 2048x128
+
+    RawImage healthBarRawImage;
+    Player player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthBarRawImage = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = -(player.HealthAsPercentage / 2f) - 0.5f;
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float MaxHealthPoints;
+
+    float CurrentHealthPoints = 100;
+
+    public float HealthAsPercentage
+    {
+        get
+        {
+            return CurrentHealthPoints / (float)MaxHealthPoints;
+        }
+    }
+}
