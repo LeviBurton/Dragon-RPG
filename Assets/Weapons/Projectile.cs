36753769241ecade4909aa6bce418041da5883fa
LeviBurton <levi.burton@gmail.com>
2018-01-29 16:40:52 -0600

    S2 L82 Projectiles discriminate

--- Assets/Weapons/Projectile.cs
+++ Assets/Weapons/Projectile.cs
@@ -2,24 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    float projectileSpeed;
+    
+    [SerializeField]
+    GameObject shooter;
 
-    public float projectileSpeed; // Note other classes can set
-
+    const float DESTROY_DELAY = 0.01f;
     float damageCaused;
+
+    public void SetShooter(GameObject shooter)
+    {
+        this.shooter = shooter;
+    }
 
     public void SetDamage(float damage)
     {
         damageCaused = damage;
     }
 
+    public float GetDefaultLaunchSpeed()
+    {
+        return projectileSpeed;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
+        var layerCollidedWith = collision.gameObject.layer;
+
+        if (layerCollidedWith != shooter.layer)
+        {
+            DamageIfDamageable(collision);
+        }
+    }
+
+    private void DamageIfDamageable(Collision collision)
+    {
         Component damagableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+
         if (damagableComponent)
         {
             (damagableComponent as IDamageable).TakeDamage(damageCaused);
         }
-        Destroy(gameObject, 0.01f);
+
+        Destroy(gameObject, DESTROY_DELAY);
     }
 }
