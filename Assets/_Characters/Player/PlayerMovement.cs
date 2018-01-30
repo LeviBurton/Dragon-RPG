06e06149bcb87fcaefd6dd0244ff6d80cf5f1731
LeviBurton <levi.burton@gmail.com>
2018-01-29 22:32:13 -0600

    Audio Trigger stuff

--- Assets/_Characters/Player/PlayerMovement.cs
+++ Assets/_Characters/Player/PlayerMovement.cs
@@ -24,8 +24,6 @@ namespace RPG.Characters
         [SerializeField] const int walkableLayerNumber = 8;
         [SerializeField] const int enemyLayerNumber = 9;
 
-        bool isInDirectMode = false;
-
         void Start()
         {
             cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
