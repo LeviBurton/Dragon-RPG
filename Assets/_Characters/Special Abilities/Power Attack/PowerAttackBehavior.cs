cd4d8e33608917a12697a9f34167bed423d39146
LeviBurton <levi.burton@gmail.com>
2018-01-30 22:23:43 -0600

    S3 L109 Storing special ability config data.

---
+++ Assets/_Characters/Special Abilities/Power Attack/PowerAttackBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    public class PowerAttackBehavior : MonoBehaviour, ISpecialAbility
+    {
+        PowerAttackConfig config;
+
+        public void SetConfig(PowerAttackConfig configToSet)
+        {
+            this.config = configToSet;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void ISpecialAbility.Use()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/_Characters/Special Abilities/Power Attack/PowerAttackConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    [CreateAssetMenu(menuName = "RPG/Special Ability/Power Attack")]
+    public class PowerAttackConfig : SpecialAbilityConfig
+    {
+        [Header("Power Attack Specific")]
+        [SerializeField]
+        float extraDamage = 10f;
+
+        public override ISpecialAbility AddComponent(GameObject gameObjectToAttachTo)
+        {
+            var behaviorComponent = gameObjectToAttachTo.AddComponent<PowerAttackBehavior>();
+            behaviorComponent.SetConfig(this);
+            return behaviorComponent;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
