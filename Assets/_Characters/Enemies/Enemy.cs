7d4f926584831eacb6593d29ca93dc93d335978b
LeviBurton <levi.burton@gmail.com>
2018-02-06 18:04:38 -0600

    S4 L145 Eliminate AICharacterControl

--- Assets/_Characters/Enemies/Enemy.cs
+++ Assets/_Characters/Enemies/Enemy.cs
@@ -27,7 +27,6 @@ namespace RPG.Characters
 
         bool isAttacking = false;
         float currentHealthPoints;
-        AICharacterControl aiCharacterControl = null;
         Player player = null;
 
         public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
@@ -41,7 +40,7 @@ namespace RPG.Characters
         void Start()
         {
             player = FindObjectOfType<Player>();
-            aiCharacterControl = GetComponent<AICharacterControl>();
+        
             currentHealthPoints = maxHealthPoints;
         }
 
@@ -69,11 +68,11 @@ namespace RPG.Characters
 
             if (distanceToPlayer <= chaseRadius)
             {
-                aiCharacterControl.SetTarget(player.transform);
+               // aiCharacterControl.SetTarget(player.transform);
             }
             else
             {
-                aiCharacterControl.SetTarget(transform);
+              //  aiCharacterControl.SetTarget(transform);
             }
         }
 
---
+++ Assets/_Characters/Player/CharacterMovement.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+using RPG.CameraUI;
+
+namespace RPG.Characters
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField] float stoppingDistance = 1.0f;
+
+        ThirdPersonCharacter character;   
+        GameObject walkTarget;
+        NavMeshAgent agent;
+
+        void Start()
+        {
+            var cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent = GetComponent<NavMeshAgent>();
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+            agent.stoppingDistance = stoppingDistance;
+
+            cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+        }
+
+        void OnMouseOverEnemy(Enemy enemy)
+        {
+            if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
+            {
+                agent.SetDestination(enemy.transform.position);
+            }
+        }
+
+        void OnMouseOverPotentiallyWalkable(Vector3 destination)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                agent.SetDestination(destination);
+            }
+        }
+
+        void Update()
+        {
+            if (agent.remainingDistance > stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        // TODO make this get called again
+        void ProcessDirectMovement()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            // calculate camera relative direction to move:
+            Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
+
+            character.Move(movement, false, false);
+        }
+    }
+}
