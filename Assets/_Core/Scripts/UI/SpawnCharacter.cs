dcdc00ba6db7625b6953bb7cd756b262bd7eba4b
Levi Burton <levi.burton@outlook.com>
2018-08-05 16:50:28 -0500

    Checking in before I add commands.

---
+++ Assets/_Core/Scripts/UI/SpawnCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour
+{
+    [SerializeField] Transform characterPrefab;
+    [SerializeField] Transform spawnPoint;
+
+    public void OnClick()
+    {
+        var characterTransform = Instantiate(characterPrefab);
+        characterTransform.transform.position = spawnPoint.position;
+    }
+}
